94d49008153d81ac6d73033591514b6bd883b859
mujeebalish <mujeebrehman12122@gmail.com>
2024-10-13 02:30:37 -0700

    hello world added

---
+++ App.js
@@ -0,0 +1,105 @@
+// ALERT
+/*alert("Welcome dear user to AlishWDev World");
+alert(' "The perpose of our life is to be happy" ');
+alert(' "Having a soft heart in a cruel world" ' + "\n" + ' "is courage, not weakness. " ');
+alert("Wecome to JS Land....");
+var confirmBox = confirm("Prevent this page from creating additional dialogs.");
+var massege = "Hello...AlishWDev" + "\n" + " Shaping Digital Futures with Apps'";
+console.log(massege);*/
+
+
+//    VARIABLES FOR STRING
+/*var username;
+var myName = "Mujeeb Rehman Developer";
+var massegeString = "Hello Welcome to AlishWDev world";
+alert( massegeString);
+var studentName = "Mujeeb Rehman Developer";
+alert( "student Name" + "\n" + studentName);
+var studentAge = 30;
+alert("Student Age" + "\n" + studentAge);
+var studentInstitute = " Saylani Mass IT Training Program";
+alert("Student Institute" + "\n" + studentInstitute);
+var pizza = "P" + "\n" + "Pi" + "\n" + "Piz" + "\n" + "pizz" + "\n" + "Pizza";
+alert(pizza)
+var email = "xyz@gmail.com ";
+var emailMassage = "My email address is ";
+alert(emailMassage + email);
+var massege5 = "Yay! Ican write HTML content through JavaScript";
+var homeDesign = '"======MÛjÈË$wyvxu"';
+alert(homeDesign);
+document.write(massege5 + "<br>" + homeDesign);*/
+
+
+// VARIABLES FOR NUMBERS
+/*var myAge = 30;
+alert("I'm " + myAge + " " + "Years old");
+var uservisiting = 25;
+document.write("Thanks for visiting this site. " + "<br>" +  "Dear visiter you have visited this site for " + uservisiting + "times" + "<br>");
+var myBirthyear = 1994;
+document.write("My birth year is " + myBirthyear  + "<br>");
+var buyerName = "Mujeeb Rehman";
+var productName = "Baby Tshirt";
+var orderQuatity = 50;
+document.write("<table border ='1px'>" + "<tr>" +
+    "<th >Name</th>" +
+    " <th>Item</th> " +
+    "<th>Quantity</th>" +
+    "</tr>"+
+     "<tr>" +
+    "<td>" +buyerName +"</td>"+
+    "<td>"+productName+"</td>"+
+    "<td>"+orderQuatity+"</td>"+
+  "</tr>"
+     +"</table>");*/
+
+    //  VARIABLES NAMES LEGAL & ILLEGAL
+    //  in one statement
+    /*var name = "Mujeeb Rehman";
+    fatherName = "Salahuddin";
+    institute = "SMIT";
+    // Legal names
+    var market_place;
+    var $currencyChance;
+    var price1;
+    var _School;*/
+    //  Illegal names
+    /*var 12345;
+    var alert;
+    var my  name;
+    var #price*/
+/*document.write("<h1>" + "Roles for naming js variables" + "</h1>")
+document.write("Variable name can contain only letters,numbers,dollar signs,and underscore." + "<br>" + "variable must begin with a " + "<u>" + "$" + "</u>" + "or" + "<u>" + "_" + "</u>" + "and letter. " + "for example $myname" +" "+"_Smit"+" "+"Saylani" + "<br>"+"variable names are cas sensitive"+" "+"variable name shouldn't be any JS keywords "); */
+
+// MATH EXPRESSIONS.
+// Addition.
+var samsungS22 = 2000;
+var samsungS23 = 3000;
+var totalMobilePrice = samsungS22 + samsungS23;
+document.write("The total price of 2 mobiles is "+totalMobilePrice);
+// Subtraction.
+var totalAmount = 5000;
+var returnAmount = 2500;
+var ramaining = totalAmount - returnAmount;
+document.write("The ramaining amount is 2500 after subtracting 2500 return amount from total amount of 5000   "+totalMobilePrice);
+// Multilication
+var tshirt = 200;
+var tshirtQuantity = 25;
+var totalTshirtAmount = tshirt*tshirtQuantity;
+console.log(" Tshirt 200. multipling with quantity amount is" + totalTshirtAmount);
+// Division
+var eide = 50000;
+var numberOfLabor = 25;
+var amountDivison = eide/numberOfLabor;
+console.log("Every labor riceived"+ amountDivison + "from 50000 Dividing in 25 number of labors" );
+// Modulus
+
+
+
+
+
+
+
+
+
+
+
